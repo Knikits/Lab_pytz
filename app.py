278fde67ef76946cad876009104df1482507234b
Knikits <nikita.kostriukov@yandex.ru>
2024-06-20 23:01:33 +0700

    convert_time

--- app.py
+++ app.py
@@ -10,3 +10,11 @@ def get_time_in_timezone(tz_name='GMT'):
         tz = pytz.timezone('GMT')
     now = datetime.now(tz)
     return now.strftime('%Y-%m-%d %H:%M:%S %Z%z')
+
+def convert_time(date_str, source_tz_str, target_tz_str):
+    source_tz = pytz.timezone(source_tz_str)
+    target_tz = pytz.timezone(target_tz_str)
+    date = datetime.strptime(date_str, '%m.%d.%Y %H:%M:%S')
+    source_time = source_tz.localize(date)
+    target_time = source_time.astimezone(target_tz)
+    return target_time.strftime('%Y-%m-%d %H:%M:%S %Z%z')
