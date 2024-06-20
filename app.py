cab68883607aa92429d827d4a313ec75827d9296
Knikits <nikita.kostriukov@yandex.ru>
2024-06-20 23:00:19 +0700

    get_time_in_timezone

--- app.py
+++ app.py
@@ -2,3 +2,11 @@ import json
 import pytz
 from datetime import datetime
 from wsgiref.simple_server import make_server
+
+def get_time_in_timezone(tz_name='GMT'):
+    try:
+        tz = pytz.timezone(tz_name)
+    except pytz.UnknownTimeZoneError:
+        tz = pytz.timezone('GMT')
+    now = datetime.now(tz)
+    return now.strftime('%Y-%m-%d %H:%M:%S %Z%z')
