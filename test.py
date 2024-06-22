fee5fdd1520fc32f52ffe6410f0a8d3d8a6920fd
Knikits <nikita.kostriukov@yandex.ru>
2024-06-22 20:42:29 +0700

    test_post_convert

--- test.py
+++ test.py
@@ -15,3 +15,11 @@ class TestApp(unittest.TestCase):
         response = self.start_response_wrapper(self.environ)
         self.assertEqual(response[0], '200 OK')
         self.assertIn('Current time in GMT', response[1].decode('utf-8'))
+    def test_post_convert(self):
+        self.environ['REQUEST_METHOD'] = 'POST'
+        self.environ['PATH_INFO'] = '/api/v1/convert'
+        data = {
+            'date': '12.20.2021 22:21:05',
+            'tz': 'EST',
+            'target_tz': 'Europe/Moscow'
+        }
