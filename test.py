07b274e016b0e1defa9ad7f30864dad8fbaa3a82
Knikits <nikita.kostriukov@yandex.ru>
2024-06-22 20:44:01 +0700

    test_post_datediff

--- test.py
+++ test.py
@@ -29,3 +29,18 @@ class TestApp(unittest.TestCase):
         self.assertEqual(response[0], '200 OK')
         response_data = json.loads(response[1].decode('utf-8'))
         self.assertIn('converted_time', response_data)
+    def test_post_datediff(self):
+        self.environ['REQUEST_METHOD'] = 'POST'
+        self.environ['PATH_INFO'] = '/api/v1/datediff'
+        data = {
+            'first_date': '12.06.2024 22:21:05',
+            'first_tz': 'EST',
+            'second_date': '12:30pm 2024-02-01',
+            'second_tz': 'Europe/Moscow'
+        }
+        self.environ['wsgi.input'] = io.BytesIO(json.dumps(data).encode('utf-8'))
+        self.environ['CONTENT_LENGTH'] = str(len(json.dumps(data)))
+        response = self.start_response_wrapper(self.environ)
+        self.assertEqual(response[0], '200 OK')
+        response_data = json.loads(response[1].decode('utf-8'))
+        self.assertIn('difference_seconds', response_data)
