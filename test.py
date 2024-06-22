d45d6d6c13db6895c8a256eab914d957d3c9d574
Knikits <nikita.kostriukov@yandex.ru>
2024-06-22 20:41:57 +0700

    test_get_root

--- test.py
+++ test.py
@@ -10,3 +10,8 @@ class TestApp(unittest.TestCase):
             'PATH_INFO': '/',
             'CONTENT_LENGTH': '0',
         }
+    def test_get_root(self):
+        self.environ['PATH_INFO'] = '/'
+        response = self.start_response_wrapper(self.environ)
+        self.assertEqual(response[0], '200 OK')
+        self.assertIn('Current time in GMT', response[1].decode('utf-8'))
