524311179b7e39dac718e1586c57dbdb0037c642
Knikits <nikita.kostriukov@yandex.ru>
2024-06-22 20:41:21 +0700

    setUp

--- test.py
+++ test.py
@@ -3,3 +3,10 @@ import unittest
 from app import application
 
 class TestApp(unittest.TestCase):
+    def setUp(self):
+        self.environ = {
+            'wsgi.input': None,
+            'REQUEST_METHOD': 'GET',
+            'PATH_INFO': '/',
+            'CONTENT_LENGTH': '0',
+        }
