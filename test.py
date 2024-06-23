80bf0a5d5f14cd1e7c2c927a5b31c900bcda7d7b
Knikits <nikita.kostriukov@yandex.ru>
2024-06-23 14:15:31 +0700

    new tests

--- test.py
+++ test.py
@@ -8,54 +8,59 @@ import threading
 from app import application
 
 class TestApp(unittest.TestCase):
-    def setUp(self):
-        self.environ = {
-            'wsgi.input': None,
-            'REQUEST_METHOD': 'GET',
-            'PATH_INFO': '/',
-            'CONTENT_LENGTH': '0',
-        }
-    def test_get_root(self):
-        self.environ['PATH_INFO'] = '/'
-        response = self.start_response_wrapper(self.environ)
-        self.assertEqual(response[0], '200 OK')
-        self.assertIn('Current time in GMT', response[1].decode('utf-8'))
-    def test_post_convert(self):
-        self.environ['REQUEST_METHOD'] = 'POST'
-        self.environ['PATH_INFO'] = '/api/v1/convert'
-        data = {
-            'date': '12.20.2021 22:21:05',
-            'tz': 'EST',
-            'target_tz': 'Europe/Moscow'
-        }
-        self.environ['wsgi.input'] = io.BytesIO(json.dumps(data).encode('utf-8'))
-        self.environ['CONTENT_LENGTH'] = str(len(json.dumps(data)))
-        response = self.start_response_wrapper(self.environ)
-        self.assertEqual(response[0], '200 OK')
-        response_data = json.loads(response[1].decode('utf-8'))
-        self.assertIn('converted_time', response_data)
-    def test_post_datediff(self):
-        self.environ['REQUEST_METHOD'] = 'POST'
-        self.environ['PATH_INFO'] = '/api/v1/datediff'
-        data = {
-            'first_date': '12.06.2024 22:21:05',
-            'first_tz': 'EST',
-            'second_date': '12:30pm 2024-02-01',
-            'second_tz': 'Europe/Moscow'
-        }
-        self.environ['wsgi.input'] = io.BytesIO(json.dumps(data).encode('utf-8'))
-        self.environ['CONTENT_LENGTH'] = str(len(json.dumps(data)))
-        response = self.start_response_wrapper(self.environ)
-        self.assertEqual(response[0], '200 OK')
-        response_data = json.loads(response[1].decode('utf-8'))
-        self.assertIn('difference_seconds', response_data)
-    def start_response_wrapper(self, environ):
-        def start_response(status, headers):
-            self.status = status
-            self.headers = headers
+    @classmethod
+    def setUpClass(cls):
+        cls.server = make_server('', 2280, validator(application))
+        cls.thread = threading.Thread(target=cls.server.serve_forever)
+        cls.thread.start()
+        
+    @classmethod
+    def tearDownClass(cls):
+        cls.server.shutdown()
+        cls.thread.join()
 
-        result = application(environ, start_response)
-        return self.status, b''.join(result)
+    def fetch_response(self, url, data=None, method='GET'):
+        request = Request(url, data=data, method=method)
+        return urlopen(request)
     
+    def test_get_gmt_time(self):
+        response = self.fetch_response('http://localhost:2280/')
+        self.assertEqual(response.status, 200)
+        self.assertIn(b'Current time in GMT', response.read())
+
+    def test_get_specific_timezone(self):
+        response = self.fetch_response('http://localhost:2280/Asia/Tel_Aviv')
+        self.assertEqual(response.status, 200)
+        self.assertIn(b'Current time in Asia/Tel_Aviv', response.read())
+        
+    def test_convert_timezone(self):
+        data = json.dumps({
+            "date": "06.15.2024 22:21:05",
+            "tz": "EST",
+            "target_tz": "Asia/Ho_Chi_Minh"
+        }).encode('utf-8')
+        response = self.fetch_response('http://localhost:2280/api/v1/convert', data=data, method='POST')
+        self.assertEqual(response.status, 200)
+        result = json.loads(response.read())
+        self.assertIn('converted_date', result)
+
+    def test_date_difference(self):
+        data = json.dumps({
+            "first_date": "06.15.2024 22:21:05", 
+            "first_tz": "CET", 
+            "second_date": "12:30pm 2024-06-15", 
+            "second_tz": "US/Hawaii"
+        }).encode('utf-8')
+        response = self.fetch_response('http://localhost:2280/api/v1/datediff', data=data, method='POST')
+        self.assertEqual(response.status, 200)
+        result = json.loads(response.read())
+        self.assertIn('seconds_difference', result)
+    
+    def test_invalid_timezone(self):
+        with self.assertRaises(HTTPError) as cm:
+            self.fetch_response('http://localhost:2280/Invalid/Timezone')
+        self.assertEqual(cm.exception.code, 404)
+        self.assertIn(b'Timezone not found', cm.exception.read())
+
 if __name__ == '__main__':
     unittest.main()
